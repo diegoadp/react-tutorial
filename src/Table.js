dc2eb5d8cdd155f463c7e2ea7987538d8bc2432b
diegoadp <83314316+diegoadp@users.noreply.github.com>
2021-10-06 09:26:46 -0300

    Feat: Create Table.js component

---
+++ src/Table.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+
+const TableHeader = () => {
+    return (
+        <tread>
+            <tr>
+                <th>Name</th>
+                <th>Job</th>
+            </tr>
+        </tread>
+    )
+}
+
+const TableBody = (props) => {
+    const rows = props.characterData.map((row, index) => {
+        return(
+            <tr key={index}>
+                <td>{row.name}</td>
+                <td>{row.job}</td>
+            </tr>
+        )
+    })
+    return <tbody>{rows}</tbody>
+}
+
+class Table extends Component {
+    render() {
+        const characterData = this.props.characterData
+
+        return (
+            <table>
+                <TableHeader />
+                <TableBody characterData={characterData} />
+            </table>
+        )
+    }
+}
+
+export default Table
