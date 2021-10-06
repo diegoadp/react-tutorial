57f06de80a2c089cddf4f62bc1f3452ee5562327
diegoadp <83314316+diegoadp@users.noreply.github.com>
2021-10-06 15:02:52 -0300

    Feat: Add delete button (learning how to deal with state)

--- src/Table.js
+++ src/Table.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 const TableHeader = () => {
     return (
@@ -13,27 +13,30 @@ const TableHeader = () => {
 
 const TableBody = (props) => {
     const rows = props.characterData.map((row, index) => {
-        return(
+
+        return (
             <tr key={index}>
                 <td>{row.name}</td>
                 <td>{row.job}</td>
+                <td><button onClick={() => props.removeCharacter(index)}>Delete</button></td>
             </tr>
         )
     })
     return <tbody>{rows}</tbody>
 }
 
-class Table extends Component {
-    render() {
-        const characterData = this.props.characterData
 
-        return (
-            <table>
-                <TableHeader />
-                <TableBody characterData={characterData} />
-            </table>
-        )
-    }
+const Table = (props) => {
+    // const { characterData, removeCharacter } = props
+    const characterData = props.characterData
+    const removeCharacter = props.removeCharacter
+
+    return (
+        <table>
+            <TableHeader />
+            <TableBody characterData={characterData} removeCharacter={removeCharacter} />
+        </table>
+    )
 }
 
 export default Table
